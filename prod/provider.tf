47adbcba83c7c2fcf8f270ccb9df4de199f7ac3c
Gerson Carneiro <gerson@linuxnaweb.com>
2023-09-15 01:29:52 +0000

    init repo

---
+++ prod/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Owner = "Gerson Carneiro"
+      Env   = "Prod"
+      Class = "CLC10"
+    }
+  }
+}
